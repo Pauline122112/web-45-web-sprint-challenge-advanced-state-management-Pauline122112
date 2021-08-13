9223cdbc3658ac7ce9e0686d7d48bde076f3afb5
Pauline122112 <pauline.design007@gmail.com>
2021-08-13 10:38:05 -0500

    added dependencies library to index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//1. Add in all necessary components and library methods.
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+
+//here is the middleware
+import { logger } from 'redux-logger'
+import { thunk } from 'redux-logger'
+
 import "./index.css";
 import App from "./App";
+
+//here is the reducer
+import reducer from './reducers'
 
 const { worker } = require('./mocks/browser');
 worker.start();
 
-const rootElement = document.getElementById("root");
+//2. Create a store that includes thunk and logger middleware support.
+const store = createStore(reducer, applyMiddleware(thunk,logger))
+
+//3. Wrap the App component in a react-redux Provider element.
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+    <App /> 
+    </Provider>,
     rootElement
-);
+    );
+    
+const rootElement = document.getElementById("root");
 
-//Task List:
-//1. Add in all necessary components and libary methods.
-//2. Create a store that includes thunk and logger middleware support.
-//3. Wrap the App component in a react-redux Provider element.
+
