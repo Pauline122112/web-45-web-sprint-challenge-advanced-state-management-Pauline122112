2627c8e06db9c42571d76589208852e2a63ee6ec
Pauline122112 <pauline.design007@gmail.com>
2021-08-13 11:58:33 -0500

    workinng on state values to initial state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,48 @@
 import axios from 'axios';
+//TASK TWO
+//1- Added a thunk action called fetchSmurfs that triggers a loading status and performs an axios call to retrieve smurfs from server, saves the result of that call to our state and shows an error if one is made.
 
-//Task List:
-//1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
+export const fetchSmurfs = () => {
+    return (dispatch) => {
+        dispatch(fetchStart())
+
+         axios.get("http://localhost:3333/smurfs")
+         .then(res=>{
+             dispatch(fetchSuccess(res.data.results[0]))
+         })
+         .catch(err => {
+             dispatch(fetchFail(err))
+         })
+    }
+}
+
+
+export const FETCH_START = "FETCH_START";
+export const fetchStart = () => {
+	return { type: FETCH_START };
+};
+
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const fetchSuccess = (smurfs) => {
+	return { type: FETCH_SUCCESS, payload: smurfs};
+};
+
+export const FETCH_FAIL = "FETCH_FAIL";
+export const fetchFail = (error) => {
+	return { type: FETCH_FAIL, payload: error };
+};
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
+export const ADD_SMURF = "ADD_SMURF"
+export const addSmurf = (newSmurf) => {
+    return { type: ADD_SMURF, payload: newSmurf}
+}
 //3. Add a standard action that allows us to set the value of the error message slice of state.
+export const ERROR_MESSAGE = "ERROR_MESSAGE"
+export const errorMessage = (error) => {
+    return { type: ERROR_MESSAGE, payload: error}
+}
+
+
+
+
+
